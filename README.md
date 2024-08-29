cde26de1b74ac24683a97d4622d911b3b11c6248
Fernando Luiz Cola <fernando.cola@emc-logic.com>
2024-08-29 15:50:11 -0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Treinamento Linux Embarcado 2024
+
+Repositório com exercícios referentes ao curso [Aprendendo Linux Embarcado na Prática com Raspberry Pi](https://cursos.embarcados.com.br/cursos/aprendendo-linux-embarcado-na-pratica-co/)
---
+++ gpio/gpio_blink.c
@@ -0,0 +1,50 @@
+// Docs on: https://libgpiod.readthedocs.io/en/latest/
+
+#include <gpiod.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define GPIO_CHIP "/dev/gpiochip0"
+#define GPIO_LINE 29
+#define SLEEP_TIME 1
+
+int main() {
+    struct gpiod_chip *chip;
+    struct gpiod_line *line;
+    int ret;
+
+    chip = gpiod_chip_open(GPIO_CHIP);
+    if (!chip) {
+        perror("Failed to open GPIO chip");
+        return 1;
+    }
+
+    line = gpiod_chip_get_line(chip, GPIO_LINE);
+    if (!line) {
+        perror("Failed to get GPIO line");
+        gpiod_chip_close(chip);
+        return 1;
+    }
+
+    ret = gpiod_line_request_output(line, "gpio_blink", 0);
+    if (ret < 0) {
+        perror("Failed to request line as output");
+        gpiod_chip_close(chip);
+        return 1;
+    }
+
+    while (1) {
+
+        gpiod_line_set_value(line, 1);
+        sleep(SLEEP_TIME);
+
+        gpiod_line_set_value(line, 0);
+        sleep(SLEEP_TIME);
+
+    }
+
+    gpiod_line_release(line);
+    gpiod_chip_close(chip);
+
+    return 0;
+}
